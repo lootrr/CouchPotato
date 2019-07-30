989281ef97aaaf7b878c756e3760eaad7b62caca
harrisonkim01 <hgk010@ucsd.edu>
2019-07-30 12:28:32 -0700

    updated stimulus to include viet language

--- stims.js
+++ stims.js
@@ -54,10 +54,86 @@ stims = [
     int_adj: ["red", "yellow", "round", "square", "dirty", "clean"],
     sub_adj: ["sweet", "bitter", "beautiful"]
   },
+
   { 
     noun: "pepper",
     int_adj: ["red", "yellow", "round", "square", "dirty", "clean"],
     sub_adj: ["sweet", "bitter", "beautiful", "ugly", "big", "small"]
   }
     
+];
+
+// used for the viet language
+viet_stims = [
+
+  // cup
+  {
+    noun: "cái ly",
+    int_adj: ["màu đỏ", "màu vàng", "hình tròn", "hình vuông", "dơ", "sạch", "nhựa", "gỗ"],
+    sub_adj: ["đẹp", "xấu", "lớn", "nhỏ", "cao", "thấp"]
+  },
+
+  // chair
+  {
+    noun: "cái ghế",
+    int_adj: ["màu đỏ", "màu vàng", "hình tròn", "hình vuông", "dơ", "sạch", "nhựa", "gỗ"],
+    sub_adj: ["đẹp", "xấu", "lớn", "nhỏ", "cao", "thấp"]
+  },
+
+  // table
+  {
+    noun: "cái bàn",
+    int_adj: ["màu đỏ", "màu vàng", "hình tròn", "hình vuông", "dơ", "sạch", "nhựa", "gỗ"],
+    sub_adj: ["đẹp", "xấu", "lớn", "nhỏ", "cao", "thấp"]
+  },
+
+  // car
+  {
+    noun: "cái xe",
+    int_adj: ["màu đỏ", "màu vàng", "hình vuông", "dơ", "sạch", "nhựa", "gỗ"],
+    sub_adj: ["đẹp", "xấu", "lớn", "nhỏ", "cao", "thấp"]
+  },
+
+  //fan
+  {
+    noun: "cái quạt",
+    int_adj: ["màu đỏ", "màu vàng", "hình tròn", "hình vuông", "dơ", "sạch", "nhựa", "gỗ"],
+    sub_adj: ["đẹp", "xấu", "lớn", "nhỏ", "cao", "thấp"]
+  },
+
+  // banana
+  {
+    noun: "trái chuối",
+    int_adj: ["màu vàng", "dơ", "sạch"],
+    sub_adj: ["ngọt", "đắng", "đẹp", "xấu", "lớn", "nhỏ"]
+  },
+
+  // mango
+  {
+    noun: "trái xoài",
+    int_adj: ["màu đỏ ", "màu vàng", "dơ", "sạch"],
+    sub_adj: ["ngọt", "đắng", "đẹp", "xấu", "lớn", "nhỏ"]
+  },
+
+  // sticky rice
+  {
+    noun: "xôi",
+    int_adj: ["màu đỏ ", "màu vàng", "dơ", "sạch"],
+    sub_adj: ["ngọt", "đắng", "đẹp", "xấu", "lớn", "nhỏ"]
+  },
+  // candy
+  {
+    noun: "cục kẹo",
+    int_adj: ["màu đỏ", "màu vàng", "hình tròn", "hình vuông", "dơ", "sạch"],
+    sub_adj: ["ngọt", "đắng", "đẹp", "lớn", "nhỏ"]
+
+  },
+  // pepper
+  {
+    noun: "trái ớt",
+    int_adj: ["màu đỏ", "màu vàng", "hình tròn", "hình vuông", "dơ", "sạch"],
+    sub_adj: ["ngọt", "đắng", "đẹp", "xấu", "lớn", "nhỏ"]
+
+  },
+  
 ];
